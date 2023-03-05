8c237c128ff06178162fe0fad779c6c1cc5de1b0
KryPat <patryk14901@gmail.com>
2023-03-05 22:42:25 +0100

    V 0.1

---
+++ app/src/main/java/com/learn/secretmessages/MainActivity.java
@@ -0,0 +1,138 @@
+package com.learn.secretmessages;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.learn.secretmessages.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtIn;
+    EditText txtOut;
+    EditText txtKey;
+    SeekBar sb;
+    Button btn;
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    public String encode( String message, int keyVal ){
+        String output = "";
+        char key = (char) keyVal;
+        for ( int x = 0; x < message.length(); x++ ) {
+            char input = message.charAt(x);
+            if (input >= 'A' && input <= 'Z')
+            {
+                input += key;
+                if (input > 'Z')
+                    input -= 26;
+                if (input < 'A')
+                    input += 26;
+            }
+            else if (input >= 'a' && input <= 'z')
+            {
+                input += key;
+                if (input > 'z')
+                    input -= 26;
+                if (input < 'a')
+                    input += 26;
+            }
+            else if (input >= '0' && input <= '9')
+            {
+                input += (keyVal % 10);
+                if (input > '9')
+                    input -= 10;
+                if (input < '0')
+                    input += 10;
+            }
+            output += input;
+        }
+        return output;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        txtIn = (EditText)findViewById(R.id.txtIn);
+        txtOut = (EditText)findViewById(R.id.txtOut);
+        txtKey = (EditText)findViewById(R.id.txtKey);
+        sb = (SeekBar)findViewById(R.id.seekBar);
+        btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int key = Integer.parseInt(txtKey.getText().toString());
+                String message = txtIn.getText().toString();
+                String output = encode(message, key);
+                txtOut.setText(output);
+            }
+        });
+
+        /*
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+         */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+     */
+}
